2c472c38060dcecdf7c63ed7da4eda22c63e3127
Michael Feinberg <mfeinberg32999@gmail.com>
2017-05-31 10:58:25 -0400

    kinda working

--- my_main.c
+++ my_main.c
@@ -1,35 +1,26 @@
 /*========== my_main.c ==========
-
   This is the only file you need to modify in order
   to get a working mdl project (for now).
-
   my_main.c will serve as the interpreter for mdl.
   When an mdl script goes through a lexer and parser, 
   the resulting operations will be in the array op[].
-
   Your job is to go through each entry in op and perform
   the required action from the list below:
-
   push: push a new origin matrix onto the origin stack
   pop: remove the top matrix on the origin stack
-
   move/scale/rotate: create a transformation matrix 
                      based on the provided values, then 
-		     multiply the current top of the
-		     origins stack by it.
-
+		          multiply the current top of the
+			       origins stack by it.
   box/sphere/torus: create a solid object based on the
                     provided values. Store that in a 
-		    temporary matrix, multiply it by the
-		    current top of the origins stack, then
-		    call draw_polygons.
-
+		        temporary matrix, multiply it by the
+			    current top of the origins stack, then
+			        call draw_polygons.
   line: create a line based on the provided values. Store 
         that in a temporary matrix, multiply it by the
 	current top of the origins stack, then call draw_lines.
-
   save: call save_extension with the provided filename
-
   display: view the image live
   
   jdyrlandweaver
@@ -48,24 +39,21 @@
 #include "display.h"
 #include "draw.h"
 #include "stack.h"
+#include "symtab.h"
 
 /*======== void first_pass() ==========
   Inputs:   
   Returns: 
-
   Checks the op array for any animation commands
   (frames, basename, vary)
   
   Should set num_frames and basename if the frames 
   or basename commands are present
-
   If vary is found, but frames is not, the entire
   program should exit.
-
   If frames is found, but basename is not, set name
   to some default value, and print out a message
   with the name being used.
-
   jdyrlandweaver
   ====================*/
 void first_pass() {
@@ -163,14 +151,11 @@ struct vary_node ** second_pass() {
   return knob;
 }
 
-
 /*======== void print_knobs() ==========
 Inputs:   
 Returns: 
-
 Goes through symtab and display all the knobs and their
 currnt values
-
 jdyrlandweaver
 ====================*/
 void print_knobs() {
@@ -193,20 +178,16 @@ void print_knobs() {
 /*======== void my_main() ==========
   Inputs: 
   Returns: 
-
   This is the main engine of the interpreter, it should
   handle most of the commadns in mdl.
-
   If frames is not present in the source (and therefore 
   num_frames is 1, then process_knobs should be called.
-
   If frames is present, the enitre op array must be
   applied frames time. At the end of each frame iteration
   save the current screen to a file named the
   provided basename plus a numeric string such that the
   files will be listed in order, then clear the screen and
   reset any other data structures that need it.
-
   Important note: you cannot just name your files in 
   regular sequence, like pic0, pic1, pic2, pic3... if that
   is done, then pic1, pic10, pic11... will come before pic2
@@ -216,45 +197,41 @@ void print_knobs() {
   x 0s in front of a number, if needed, so if used correctly,
   and x = 4, you would get numbers like 0001, 0002, 0011,
   0487
-
   jdyrlandweaver
   ====================*/
+
 void my_main() {
 
-  int i;
+  int i, j, counter;
   struct matrix *tmp;
   struct stack *systems;
   screen t;
   color g;
   double step = 0.1;
-  double theta;
-  double knob_val;
+  double theta,k_value;
+  
   systems = new_stack();
   tmp = new_matrix(4, 1000);
   clear_screen( t );
   g.red = 0;
   g.green = 0;
   g.blue = 0;
-  //printf("running first pass\n");
-  first_pass();
-  //printf("ran first pass\n");
+
+  num_frames = 1;
   struct vary_node * knob;
-  struct vary_node ** knob_list = (struct vary_node **)malloc(sizeof(struct vary_node*)*num_frames);
-  if(num_frames>1){
-    //printf("running second pass\n");
-    knob_list = second_pass();
-    //printf("ran second pass\n");
+  struct vary_node ** knobsCur = (struct vary_node **)malloc(sizeof(struct vary_node*)*num_frames);
+  first_pass();
+  if (num_frames > 1){
+    knobsCur = second_pass();
   }
-  print_knobs();
-  int j;
-  for(j = 0; j < num_frames; j++){
-    if(num_frames>1){
-      for(knob = knob_list[j]; knob != NULL; knob = knob->next){
+
+  for (counter = 0; counter < num_frames; counter++) {
+    if (num_frames > 1) {
+      for(knob = knobsCur[counter]; knob != NULL; knob = knob->next){
 	set_value(lookup_symbol(knob->name),knob->value);
       }
+
     }
-    printf("set knob values\n");
-    
     for (i=0;i<lastop;i++) {
       
       printf("%d: ",i);
@@ -301,7 +278,7 @@ void my_main() {
 		    op[i].op.torus.r0,op[i].op.torus.r1, step);
 	  matrix_mult( peek(systems), tmp );
 	  draw_polygons(tmp, t, g);
-	  tmp->lastcol = 0;	  
+	  tmp->lastcol = 0;  
 	  break;
 	case BOX:
 	  printf("Box: d0: %6.2f %6.2f %6.2f d1: %6.2f %6.2f %6.2f",
@@ -352,11 +329,11 @@ void my_main() {
 	  if (op[i].op.move.p != NULL)
 	    {
 	      printf("\tknob: %s",op[i].op.move.p->name);
-	      knob_val = op[i].op.move.p->s.value;
+	      k_value = op[i].op.move.p -> s.value;
 	    }
-	  tmp = make_translate( op[i].op.move.d[0]*knob_val,
-				op[i].op.move.d[1]*knob_val,
-				op[i].op.move.d[2]*knob_val);
+	  tmp = make_translate( op[i].op.move.d[0] * k_value,
+				op[i].op.move.d[1] * k_value,
+				op[i].op.move.d[2] * k_value);
 	  matrix_mult(peek(systems), tmp);
 	  copy_matrix(tmp, peek(systems));
 	  tmp->lastcol = 0;
@@ -368,18 +345,14 @@ void my_main() {
 	  if (op[i].op.scale.p != NULL)
 	    {
 	      printf("\tknob: %s",op[i].op.scale.p->name);
-	      knob_val = op[i].op.move.p->s.value;
-	      printf("\tset knob");
+	      k_value = op[i].op.scale.p -> s.value;
 	    }
-	  tmp = make_scale( op[i].op.scale.d[0]*knob_val,
-			    op[i].op.scale.d[1]*knob_val,
-			    op[i].op.scale.d[2]*knob_val);
-	  printf("\tset tmp");
+	  tmp = make_scale( op[i].op.scale.d[0] * k_value,
+			    op[i].op.scale.d[1] * k_value,
+			    op[i].op.scale.d[2] * k_value);
 	  matrix_mult(peek(systems), tmp);
-	  
 	  copy_matrix(tmp, peek(systems));
 	  tmp->lastcol = 0;
-	  printf("finished scale");
 	  break;
 	case ROTATE:
 	  printf("Rotate: axis: %6.2f degrees: %6.2f",
@@ -388,16 +361,16 @@ void my_main() {
 	  if (op[i].op.rotate.p != NULL)
 	    {
 	      printf("\tknob: %s",op[i].op.rotate.p->name);
-	      knob_val = op[i].op.move.p->s.value;
+	      k_value = op[i].op.rotate.p -> s.value;
 	    }
-	  theta =  op[i].op.rotate.degrees*knob_val * (M_PI / 180);
+	  theta =  op[i].op.rotate.degrees * (M_PI / 180) * k_value;
 	  if (op[i].op.rotate.axis == 0 )
 	    tmp = make_rotX( theta );
 	  else if (op[i].op.rotate.axis == 1 )
 	    tmp = make_rotY( theta );
 	  else
 	    tmp = make_rotZ( theta );
-	  
+	    
 	  matrix_mult(peek(systems), tmp);
 	  copy_matrix(tmp, peek(systems));
 	  tmp->lastcol = 0;
@@ -429,15 +402,14 @@ void my_main() {
       printf("\n");
     }
     if(num_frames>1){
-      printf("reaches animation\n");
       char temp [128];
-      sprintf(temp ,"anim/%s%03d.png",name,j);
-      save_extension(t,temp);
+      sprintf(temp ,"anim/%s%03d.png",name,counter);
+      save_extension(t,temp );
       clear_screen(t);
       free_stack(systems);
       tmp ->lastcol = 0;
       systems = new_stack();
-    } 
+    }
   }
   make_animation(name);
 }
